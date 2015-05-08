4b1772e8107a0a9973eb223f34da459848f4c55c
pinto <mpinter.pinto@gmail.com>
2015-05-08 03:12:08 +0200

    first draw

--- som.py
+++ som.py
@@ -1,5 +1,8 @@
 import random
+import scipy.io
 import math
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 alpha=0.5
 lamb=0.8
@@ -16,7 +19,17 @@ def randlist(n):
         data[-1].append(random.random())
         data[-1].append(random.random())
         data[-1].append(random.random())
-        data[-1].append(random.randint(0,2))
+        #data[-1].append(random.randint(0,2))
+
+def matlist():
+    test=scipy.io.loadmat('SOM_data.mat')
+    print(test['data'])
+    rand_size=test['data'][0].size
+    for i in range(test['data'][0].size):
+        data.append([])
+        data[-1].append(test['data'][0][i])
+        data[-1].append(test['data'][1][i])
+        data[-1].append(test['data'][2][i])
 
 def randnet(n):
     for i in range(n):
@@ -36,11 +49,12 @@ def omega1(k1,l1,k2,l2,lamb):
     return pow(lamb,max(abs(k2-k1),abs(l2-l1)))
 
 def omega2(k1,l1,k2,l2,lamb):
-    return exp(-1*dist(w[k1][l1],w[k2][l2])^2)/lamb*lamb
+    return math.exp(-1.0*pow(dist(w[k1][l1],w[k2][l2]),2))/(lamb*lamb)
 
-randlist(rand_size)
+matlist()
+#randlist(rand_size)
 randnet(w_size)
-for ep in range(10):
+for ep in range(100):
     perm=list(range(rand_size))
     random.shuffle(perm)
     for j in perm:
@@ -60,5 +74,26 @@ for ep in range(10):
                 wadd=[omega1(mink,minl,k,l,lamb)*alpha*(xi-wi) for xi,wi in zip(x,w[k][l])]
                 for i in range(3):
                     w[k][l][i]+=wadd[i]
-    alpha=alpha-0.001;
-    lamb=lamb-0.003;
+    alpha=alpha-0.001
+    lamb=lamb-0.003
+print(w)
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+x=[]
+y=[]
+z=[]
+dx=[]
+dy=[]
+dz=[]
+for k in range(w_size):
+    for l in range(w_size):
+        x.append(w[k][l][0])
+        y.append(w[k][l][1])
+        z.append(w[k][l][2])
+for i in range(rand_size):
+    dx.append(data[i][0])
+    dy.append(data[i][1])
+    dz.append(data[i][2])
+ax.plot_trisurf(x,y,z)
+ax.scatter(dx,dy,dz)
+plt.show()
